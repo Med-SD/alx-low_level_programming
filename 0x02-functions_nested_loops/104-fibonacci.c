70e6f2bce0c24e69bf820d94d2617229eca09af0
Med SD <saadimohammed98@gmail.com>
2023-06-20 12:04:13 -0700

    init file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,38 +7,22 @@
  */
 int main(void)
 {
-	unsigned long int a = 0, b = 1, c;
-	unsigned long int d, e, f, g;
-	int h = 0;
+    unsigned long int current = 1;
+    unsigned long int previous = 1;
+    unsigned long int temp;
+    int count;
 
-	for (h = 0; h <= 91; h++)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		printf("%lu, ", c);
-	}
-	d = a % 1000;
-	a = a / 1000;
-	e = b % 1000;
-	b = b / 1000;
-	while (h <= 98)
-	{
-		g = (d + e) / 1000;
-		f = (d + e) - g * 1000;
-		c = (a + b) + g;
-		d = e;
-		e = f;
-		a = b;
-		b = c;
-		if (f >= 100)
-			printf("%lu%lu", c, f);
-		else
-			printf("%lu0%lu", c, f);
-		if (h != 98)
-			printf(" , ");
-		h++;
-		}
-	printf("\n");
-	return (0);
+    printf("%lu, %lu", previous, current);
+
+    for (count = 3; count <= 98; count++)
+    {
+        temp = current;
+        current += previous;
+        previous = temp;
+
+        printf(", %lu", current);
+    }
+
+    printf("\n");
+    return (0);
 }
