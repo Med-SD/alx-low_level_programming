cd5fb9c9d5ed256f864135e24d42ebeaaa42ff2d
Med SD <saadimohammed98@gmail.com>
2023-06-20 12:47:46 -0700

    init file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,11 +5,11 @@
  * @n: An input integer value
  * Return: Nothing
  */
-int print_times_table(int n)
+void print_times_table(int n)
 {
 	int i, j;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -18,9 +18,7 @@ int print_times_table(int n)
 				putformat(i * j);
 			_putchar('\n');
 		}
-	return (0);
 	}
-	return (0);
 }
 
 /**
