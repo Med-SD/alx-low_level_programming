e0635bfa6539c3780f872336f16bf660020dc127
Med SD <saadimohammed98@gmail.com>
2023-07-11 15:36:02 -0700

    init file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,11 +17,17 @@ int **alloc_grid(int width, int height)
 
 	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
+		free(array);
 		return (NULL);
 	for (; i < height; i++)
 	{
 		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+			for (j = i; j >= 0; j--)
+			{
+				free(array[j]);
+			}
+			free(array);
 			return (NULL);
 	}
 
